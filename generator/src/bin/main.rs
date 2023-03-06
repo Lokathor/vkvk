2a59440115ede26894559a7e83582b6068f90bb0
Lokathor <zefria@gmail.com>
2023-03-06 10:38:59 -0700

    work with bitmasks.

--- generator/src/bin/main.rs
+++ generator/src/bin/main.rs
@@ -5,6 +5,10 @@ const XML: &str = include_str!("../../vk.xml");
 
 fn main() {
   let registry = VulkanRegistry::from_static_str(XML);
+  //
   let enumerations = gather_enumerations(&registry);
   write_out_enumerations("../src/generated/enumerations.rs", &enumerations);
+  //
+  let bitmasks = gather_bitmasks(&registry);
+  println!("{bitmasks:#?}");
 }
